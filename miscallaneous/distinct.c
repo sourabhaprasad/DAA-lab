05e0a3e25e8e08d394293b910a0ec6cc597d975a
Sourabha K H <sourabhaprasad04@gmail.com>
2024-06-27 22:25:12 +0530

    program

---
+++ miscallaneous/distinct.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printDistinct(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // Check if the picked element is already printed
+        int j;
+        for (j = 0; j < i; j++)
+            if (arr[i] == arr[j])
+                break;
+
+        // If not printed earlier, then print it
+        if (i == j)
+            printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {6, 10, 5, 4, 9, 120, 4, 6, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printDistinct(arr, n);
+    return 0;
+}
